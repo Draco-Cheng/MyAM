e6fd47840f405dd0d36980f4dcf155723730bf72
Anonymous <private@google.com>
2017-04-19 18:38:19 +1000

    [Server] add limit length on server print console [Server] add cross domain check on server side ( method: OPTION )

--- routes/index.js
+++ routes/index.js
@@ -10,23 +10,38 @@ var responseHandler = require('../controller/responseHandler.js');
  */
 
 routes.index = function(req, res, next) {
-	if ('do next'){
-		next();
-	}else{
-		responseHandler(401, req, res);
-	}
+  if (req.method === 'OPTIONS') {
+    console.log('!OPTIONS');
+    var headers = {};
+    // IE8 does not allow domains to be specified, just the *
+    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+    headers["Access-Control-Allow-Origin"] = "*";
+    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+    headers["Access-Control-Allow-Credentials"] = false;
+    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+    res.writeHead(200, headers);
+    res.end();
+  } else if ('do next') {
+  	// Cross domain header
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  } else {
+    responseHandler(401, req, res);
+  }
 }
 
-routes.ping = function(req, res,next) {
-	responseHandler(200, req, res);
+routes.ping = function(req, res, next) {
+  responseHandler(200, req, res);
 }
-	
-router.all('*'		, routes.index)
-	  .head('/'		, routes.ping)
-	  .get('/ping'	, routes.ping)
-	  .use('/db'	, require('./db'))
-	  .use('/currency'	, require('./currency'))
-	  .use('/type'	, require('./type'))
-	  .use('/record'	, require('./record'));
+
+router.all('*', routes.index)
+  .head('/', routes.ping)
+  .get('/ping', routes.ping)
+  .use('/db', require('./db'))
+  .use('/currency', require('./currency'))
+  .use('/type', require('./type'))
+  .use('/record', require('./record'));
 
 module.exports = router;
