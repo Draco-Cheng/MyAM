de23b27550ecaad5edf924f8ab46b5df96a25362
Anonymous <private@google.com>
2017-04-22 11:22:05 +1000

    rename "directive" folder to "_directive"

--- public/myam/src/app/components/records/records.module.ts
+++ public/myam/src/app/components/records/records.module.ts
@@ -5,8 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { RecordsComponent } from './records.component';
 import { ChildRoutingModule } from './records.routing';
 
-import { RecordTableDirectiveComponent } from './directive/record-table.directive.component';
-import { RecordTypeMapDirectiveComponent } from './directive/record-type-map.directive.component';
+import { RecordTableDirectiveComponent } from './_directive/record-table.directive.component';
+import { RecordTypeMapDirectiveComponent } from './_directive/record-type-map.directive.component';
 
 @NgModule({
   imports: [
