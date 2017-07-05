1b918fe0e1f66a3f46562f2bdc328739a337d983
Anonymous <private@google.com>
2017-07-05 19:35:33 +0800

    [UI] separate image and css out of bundle.js

--- public/myam/webpack.config.js
+++ public/myam/webpack.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const isDev = !!process.env.dev;
+
+let setting = {
   entry: "./src/index.js",
   output: {
     path: __dirname + "/src/dist",
@@ -20,18 +22,45 @@ module.exports = {
       test: /\.html$/,
       loader: 'raw-loader',
       exclude: /node_modules/
-    }, {
-      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-      loader: 'url-loader?limit=100000'
-    }, {
-      test: /\.less$/,
-      use: [{
-        loader: "style-loader" // creates style nodes from JS strings
-      }, {
-        loader: "css-loader" // translates CSS into CommonJS
-      }, {
-        loader: "less-loader" // compiles Less to CSS
-      }]
     }]
-  }
+  },
+  plugins: []
 };
+
+if (isDev) {
+  let _addLoaders = [{
+    test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+    loader: 'url-loader?limit=100000'
+  }, {
+
+    test: /\.(css|less)$/,
+    use: [{
+      loader: "style-loader" // creates style nodes from JS strings
+    }, {
+      loader: "css-loader" // translates CSS into CommonJS
+    }, {
+      loader: "less-loader" // compiles Less to CSS
+    }]
+  }];
+  setting['module']['loaders'].push(..._addLoaders);
+} else {
+  const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+  const extractCSS = new ExtractTextPlugin('[name].css');
+  const extractLESS = new ExtractTextPlugin('[name]-less.css');
+
+  let _addLoaders = [{
+    test: /\.(jpe?g|gif|png|svg|woff|woff2|eot|ttf|wav|mp3)$/,
+    loader: "file-loader"
+  }, {
+    test: /\.css$/,
+    use: extractCSS.extract(['css-loader', 'postcss-loader'])
+  }, {
+    test: /\.less$/i,
+    use: extractLESS.extract(['css-loader', 'less-loader'])
+  }];
+
+  setting['module']['loaders'].push(..._addLoaders);
+  setting['plugins'].push(extractCSS, extractLESS);
+}
+module.exports = setting;
